9ee1d52e48f328f92695f0ef68a406566d606807
gurinder <guribanwait07@gmail.com>
2018-05-18 13:46:48 -0400

    lab2 part 2 updated

--- Lab2/lab2.js
+++ Lab2/lab2.js
@@ -29,8 +29,30 @@ var earthPhysics;
     earthPhysics[earthPhysics["c"] = Math.pow(3.88, 8)] = "c";
     earthPhysics[earthPhysics["weight"] = 1] = "weight";
 })(earthPhysics || (earthPhysics = {}));
-console.log(days[0]);
 //html elements
 var pTodayDate = document.getElementById("p--today-date");
+var birthButton = document.getElementById("button--birthday");
+var getBdate = document.getElementById("input--date-picker");
+var result = document.getElementById("p--birthday-message");
+//part 1
 var today = new Date();
 pTodayDate.innerHTML = "Today is " + days[today.getDay()] + ", \n                        " + months[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
+//part2
+function getBday(bdate) {
+    //if today is user's bday
+    if ((bdate.getDate() + 1 === today.getDate()) && (bdate.getMonth() === today.getMonth())) {
+        return 'Happy Birthday';
+    }
+    var thisYearsBirthday = new Date();
+    thisYearsBirthday.setDate(bdate.getDate());
+    thisYearsBirthday.setMonth(bdate.getMonth());
+    thisYearsBirthday.setFullYear(today.getFullYear());
+    return "Your Birthday will be on " + days[bdate.getDay()] + " " + months[bdate.getMonth()] + " " + (bdate.getDate() + 1) + ", " + thisYearsBirthday.getFullYear();
+}
+birthButton.onclick = function () {
+    //get bday from input
+    var userBDay = getBdate.value;
+    var userBDayDate = new Date(userBDay);
+    //output
+    result.innerHTML = getBday(userBDayDate);
+};
--- Lab2/lab2.ts
+++ Lab2/lab2.ts
@@ -26,9 +26,35 @@ enum earthPhysics {
     c = Math.pow(3.88, 8),
     weight = 1
 }
-console.log(days[0]);
 //html elements
 let pTodayDate = document.getElementById("p--today-date");
+let birthButton = document.getElementById("button--birthday");
+let getBdate = document.getElementById("input--date-picker");
+let result = document.getElementById("p--birthday-message");
+
+//part 1
 let today : Date = new Date();
 pTodayDate.innerHTML = `Today is ${days[today.getDay()]}, 
                         ${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`;
+//part2
+function getBday(bdate : Date) : string {
+    //if today is user's bday
+    if((bdate.getDate() + 1 === today.getDate()) && (bdate.getMonth() === today.getMonth())) {
+        return 'Happy Birthday';
+    }
+        let thisYearsBirthday = new Date();
+        thisYearsBirthday.setDate(bdate.getDate());
+        thisYearsBirthday.setMonth(bdate.getMonth());
+        thisYearsBirthday.setFullYear(today.getFullYear());
+        return `Your Birthday will be on ${days[bdate.getDay()]} ${months[bdate.getMonth()]} ${bdate.getDate() + 1 }, ${thisYearsBirthday.getFullYear()}`
+}
+birthButton.onclick = function() {
+    //get bday from input
+    let userBDay : string = getBdate.value;
+    let userBDayDate : Date = new Date(userBDay);
+    //output
+    result.innerHTML = getBday(userBDayDate);
+}
+
+
+
