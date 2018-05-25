0f63962c3bd0a28b7125860160e99439a283ccea
gurinder <guribanwait07@gmail.com>
2018-05-25 15:17:36 -0400

    lab3 part 2 added

--- Lab3/lab3.js
+++ Lab3/lab3.js
@@ -1,6 +1,65 @@
 var map;
+var index = 0;
+//let coolLoc : any[] = [];
+var mapMarkers = [];
+//class
+var MapMarker = /** @class */ (function () {
+    function MapMarker(address) {
+        this.Address = address;
+    }
+    ;
+    return MapMarker;
+}());
 var Toronto = { lat: 43.653226, lng: -79.38318429999998 };
 var initMapConfig = { center: Toronto, zoom: 8 };
+$.ajax({
+    url: './locations.json',
+    datatype: 'json',
+    success: function (data) {
+        for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
+            var cl = data_1[_i];
+            var newMapMarker = new MapMarker(cl.address);
+            mapMarkers.push(newMapMarker);
+        }
+    }
+});
+window.onload = function () {
+    addMarkerWithGeocoder();
+};
 function initMap() {
+    var geocoder = new google.maps.Geocoder();
     map = new google.maps.Map(document.getElementById('map'), initMapConfig);
+    //addMarker(Toronto);
+    //getLatLng("1 Yonge Street Toronto, Ontario, Canada");
+    /*for(let cl of coolLoc) {
+        let newMapMarker : MapMarker = new MapMarker(cl.address);
+        mapMarkers.push(newMapMarker);*/
+}
+function addMarkerWithGeocoder() {
+    var resultLatlng = { lat: 0, lng: 0 };
+    geocoder = new google.maps.Geocoder();
+    if (index < mapMarkers.length) {
+        console.log(index);
+        var address = mapMarkers[index].Address;
+        geocoder.geocode({ 'address': address }, function (results, status) {
+            if (status == 'OK') {
+                index++;
+                resultLatlng.lat = results[0].geometry.location.lat();
+                resultLatlng.lng = results[0].geometry.location.lng();
+                addMarker(resultLatlng);
+                addMarkerWithGeocoder();
+            }
+            else {
+                setTimeout(function () { addMarkerWithGeocoder(); }, 500);
+            }
+        });
+    }
+}
+function addMarker(coord) {
+    //will place map marker based on coordinates
+    var newMarker = new google.maps.Marker({
+        position: coord,
+        map: map,
+        title: "A cool place to be"
+    });
 }
--- Lab3/lab3.ts
+++ Lab3/lab3.ts
@@ -1,17 +1,85 @@
 let map : any;
-
+let index = 0;
+//let coolLoc : any[] = [];
+let mapMarkers : MapMarker[] = [];
 //interface
 interface LatLng {
     lat : number,
     lng : number
 }
-interface GoogleMapsConfig {
-    center : LatLng,
+interface GoogleMapsConfig{
+    center: string,
     zoom : number
+}
+//class
+class MapMarker {
+    Address : string;
+    LatLng : LatLng;
+    public constructor(address: string){
+        this.Address = address;
+    };
 }
 
 let Toronto : LatLng = { lat: 43.653226, lng: -79.38318429999998};
-let initMapConfig = { center: Toronto, zoom: 8};
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), initMapConfig);
+let initMapConfig : GoogleMapsConfig = { center: Toronto, zoom: 8};
+
+$.ajax ({
+    url: './locations.json',
+    datatype: 'json',
+    success: function (data) {
+        for(let cl of data) {
+            let newMapMarker : MapMarker = new MapMarker(cl.address);
+            mapMarkers.push(newMapMarker);
+        }
+    }
+});
+
+window.onload = function () {
+    addMarkerWithGeocoder();
 }
+function initMap() {
+    let geocoder : object = new google.maps.Geocoder();
+    map = new google.maps.Map(
+        document.getElementById('map'), initMapConfig
+    );
+    //addMarker(Toronto);
+    //getLatLng("1 Yonge Street Toronto, Ontario, Canada");
+    /*for(let cl of coolLoc) {
+        let newMapMarker : MapMarker = new MapMarker(cl.address);
+        mapMarkers.push(newMapMarker);*/
+
+    }
+function addMarkerWithGeocoder() {
+    let resultLatlng : LatLng = {lat: 0, lng: 0};
+
+    geocoder = new google.maps.Geocoder();
+
+    if (index < mapMarkers.length) {
+        console.log(index);
+        let address : string = mapMarkers[index].Address;
+        geocoder.geocode(
+            {'address': address},
+            function (results, status) {
+
+                if (status == 'OK') {
+                    index ++;
+                    resultLatlng.lat = results[0].geometry.location.lat();
+                    resultLatlng.lng = results[0].geometry.location.lng();
+                    addMarker(resultLatlng);
+                    addMarkerWithGeocoder();
+                } else {
+                    setTimeout(function(){addMarkerWithGeocoder()}, 500);
+                }
+            }
+        );
+    }
+}
+    function addMarker(coord : LatLng) : void {
+        //will place map marker based on coordinates
+        let newMarker =  new google.maps.Marker({
+            position: coord,
+            map: map,
+            title: `A cool place to be`
+        });
+    }
+
